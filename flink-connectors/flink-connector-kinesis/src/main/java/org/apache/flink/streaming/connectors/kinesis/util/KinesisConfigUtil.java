0020a496487f9b9c5fa5d6d22b633f6cbff5abea
Addison Higham <ahigham@instructure.com>
2018-07-03 17:12:21 -0600

    [FLINK-9618] [kinesis] Allow endpoint to be sent to KPL
    This code changes the kinesis adapter so that it works for BOTH the
    kinesis client and the KPL
    
    This does a few things:
    1. Custom endpoints were never being passed onto the KPL, this fixes
    that
    2. This removes the check for an exclusive region or endpoint and
    instead makes a preference for setting endpoint first, followed by
    region if set
    3. It always passes endpoint AND region, as both are required by the KPL
    4. Removes some tests that assumes that a region must be set. If no
    region is set, the kinesis SDK and the KPL should properly use the
    fallback region

--- flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/util/KinesisConfigUtil.java
+++ flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/util/KinesisConfigUtil.java
@@ -1,12 +1,12 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements.	See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *		http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,11 +26,15 @@ import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConsta
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants.InitialPosition;
 import org.apache.flink.streaming.connectors.kinesis.config.ProducerConfigConstants;
 
+import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 import java.util.Properties;
 
 import static org.apache.flink.util.Preconditions.checkArgument;
@@ -191,7 +195,20 @@ public class KinesisConfigUtil {
 		validateAwsConfiguration(config);
 
 		KinesisProducerConfiguration kpc = KinesisProducerConfiguration.fromProperties(config);
-		if (config.containsKey(AWSConfigConstants.AWS_REGION)) {
+		if (config.containsKey(AWSConfigConstants.AWS_ENDPOINT)) {
+			try {
+				URI kinesisEndpoint = new URI(config.getProperty(AWSConfigConstants.AWS_ENDPOINT));
+				// fallback value, this shouldn't matter when we have an endpoint set, but we need it for KPL
+				// to be happy
+				String curRegion = Optional.ofNullable(Regions.getCurrentRegion()).map(Region::toString).orElse("us-east-1");
+				String region = Optional.ofNullable(config.getProperty(AWSConfigConstants.AWS_REGION)).orElse(curRegion);
+				kpc.setRegion(region);
+				kpc.setKinesisEndpoint(kinesisEndpoint.getHost());
+				kpc.setKinesisPort(kinesisEndpoint.getPort());
+			} catch (URISyntaxException e) {
+				throw new IllegalArgumentException("invalid AWS_ENDPOINT value, must be a valid URI", e);
+			}
+		} else if (config.containsKey(AWSConfigConstants.AWS_REGION)) {
 			kpc.setRegion(config.getProperty(AWSConfigConstants.AWS_REGION));
 		}
 		kpc.setCredentialsProvider(AWSUtil.getCredentialsProvider(config));
@@ -242,12 +259,6 @@ public class KinesisConfigUtil {
 						"and Secret Key ('" + AWSConfigConstants.AWS_SECRET_ACCESS_KEY + "') when using the BASIC AWS credential provider type.");
 				}
 			}
-		}
-
-		if (!(config.containsKey(AWSConfigConstants.AWS_REGION) ^ config.containsKey(ConsumerConfigConstants.AWS_ENDPOINT))) {
-			// per validation in AwsClientBuilder
-			throw new IllegalArgumentException(String.format("Either AWS region ('%s') or AWS endpoint ('%s') must be set in the config.",
-				AWSConfigConstants.AWS_REGION, AWSConfigConstants.AWS_REGION));
 		}
 
 		if (config.containsKey(AWSConfigConstants.AWS_REGION)) {
